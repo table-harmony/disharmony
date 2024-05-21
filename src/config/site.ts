502a511e6fc96151a16ec573e64b42e7885ba857
table-harmony <lironkaner2007@gmail.com>
2024-05-21 21:48:17 +0300

    Init shadcn

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "disharmony",
+  url: "https://disharmony.vercel.app",
+  author: "tableharmony",
+  description: "discord clone",
+  links: {
+    github: "https://github.com/table-harmony/disharmony",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
